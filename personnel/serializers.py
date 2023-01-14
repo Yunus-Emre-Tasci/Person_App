3c566c19dcdd37298e7307e13f046d7f52d025f9
YUNUS EMRE TASCİ <yunusemretasci092@gmail.com>
2023-01-14 12:19:29 +0300

    permissionsIsStaffOrReadOnly

---
+++ personnel/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .models import Department,Personnel
+
+class DepartmentSerializer(serializers.ModelSerializer):
+    
+    personnel_count=serializers.SerializerMethodField()
+    
+    class Meta:
+        model=Department
+        fields=("id","name","personnel_count")
+        
+    def get_personnel_count(self,obj):
+        return obj.personals.count()    
+    
+
+class PersonnelSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model=Personnel
+        fields="__all__"    
--- personnel/views.py
+++ personnel/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .serializers import DepartmentSerializer,PersonnelSerializer
+from rest_framework import generics
+from .models import Department,Personnel
+from .permissions import IsStafforReadOnly
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
+
+class DepartmentView(generics.ListCreateAPIView):
+    queryset=Department.objects.all()
+    serializer_class=DepartmentSerializer
+    permission_classes=[IsStafforReadOnly,IsAuthenticated]
+    
+
+class PersonnelListCreateView(generics.ListCreateAPIView):
+    queryset=Personnel.objects.all()
+    serializer_class=PersonnelSerializer    
